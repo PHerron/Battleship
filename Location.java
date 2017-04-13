6ecee54ed9d4f8cffe7230f8faa8c269d7401f51
PHerron <pherron@stu.jsu.edu>
2017-04-13 16:46:48 -0500

    Added vars to location
    ;sehklje

--- Location.java
+++ Location.java
@@ -1,5 +1,15 @@
 /// Location on Grid
 
+/**
+ * Individual cell of Grid
+ * Keep track of own place on grid (?)
+ * Enum type
+ * Boolean firedOn
+ */
 public class Location {
+	public enum type {EMPTY, SHIPHEAD, SHIPBODY};
+	private int id;
+	private boolean firedOn;
+	
 	
 }
