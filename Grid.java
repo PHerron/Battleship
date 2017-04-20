e7f79723a10a1d96f53b2d03ba60d5c5089fb5d8
jlanders2 <colelanders@Coles-MacBook-Pro.local>
2017-04-20 16:25:03 -0500

    Syntax 4.0
    asdfasdfasdf

--- Grid.java
+++ Grid.java
@@ -1,37 +1,37 @@
 /// Grid
- 
+
 import java.util.*;
- 
+
 public class Grid extends Observable {
     public enum Result {NONE, WIN, LOSE};
- 
+
     // Grid of Locations
     private Location[][] location;
- 
+
     // Battleship length = width, so this works for both dimensions
     private static final int GRIDSIZE = 10;
- 
+
     // Array for the sizes of each individual ship; will have something to do with location.id
     private static final int[] SHIPSIZES = {5, 4, 3, 3, 2};
- 
+
     private Random random;
-   
+
     private boolean[] shipAliveStatus;
- 
+
     public Grid() {
         location = new Location[GRIDSIZE][GRIDSIZE];
         random = new Random();
         shipAliveStatus = {true,true,true,true,true};
- 
+
         for(int i = 0; i < GRIDSIZE; i++){
             for(int j = 0; j < GRIDSIZE; j++){
                 location[i][j] = new Location();
             }
         }
- 
+
         deployFleet();
     }
- 
+
     public void deployFleet() {
         for(int j=1;j<=SHIPSIZES.length;j++)
         {
@@ -44,8 +44,8 @@ public class Grid extends Observable {
                 vertical = random.nextBoolean();
             }
             while(!isValidLocation(y,x,vertical,SHIPSIZES[i]);
-           
-            if(vertical)   
+
+            if(vertical)
                 for(int i=0;i<shipLength;i++)
                     location[row+i][col].setId(i);
             else
@@ -53,11 +53,11 @@ public class Grid extends Observable {
                     location[row][col+i].setId(i);
         }
     }
- 
+
     public boolean isValidLocation(int row, int col, boolean vertical, int shipLength) {
         if(!(vertical ? ((row+shipLength)<10) : ((col+shipLength)<10)))
             return false;
-        if(vertical)   
+        if(vertical)
             for(int i=0;i<shipLength;i++)
                 if(location[row+i][col].getId() != 0)
                     return false;
@@ -67,26 +67,26 @@ public class Grid extends Observable {
                     return false;
         return true;
     }
- 
+
     public Result getResult() {
         for(int i = 0; i < 10; i++) {
             for(int j = 0; j < 10; j++) {
-                if((location[i][j].getID != 0) && !location[i][j].isFiredOn()) {
+                if((location[i][j].getId() != 0) && !location[i][j].isFiredOn()) {
                     return Result.NONE;
                 }
             }
         }
         return Result.LOSE;
     }
- 
+
     public Location getLocation(int row, int col) {
         return location[row][col];
     }
- 
+
     public boolean isHit(int row, int col) {
         return (getLocation(row, col).isFiredOn() && (getLocation(row, col).getId() != 0))
     }
- 
+
     // for Observable
     public void fireOn(int row, int col) {
         if(location[row][col].isFiredOn()) return;
@@ -96,7 +96,7 @@ public class Grid extends Observable {
         setChanged();
         notifyObservers("firedOn:"+row+":"+col);
     }
- 
+
     // for Observable
     public void checkSunkShip() {
 <<<<<<< HEAD
@@ -114,7 +114,7 @@ public class Grid extends Observable {
             }
         }
 =======
-       
+
 >>>>>>> origin/master
     }
 }
